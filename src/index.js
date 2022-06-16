439da95008f20f2a6078486d010be127809ff682
rjadyshkom <sergey@rjadysh.com>
2022-06-16 23:25:12 +0300

    feat: actions, reducers, store initialize

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './styles/global.css';
 import App from './components/app/App';
 import { BrowserRouter } from 'react-router-dom';
+import { legacy_createStore as createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { rootReducer } from './services/reducers';
 
+const store = createStore(rootReducer);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
 );
---
+++ src/services/reducers/cart.js
@@ -0,0 +1,28 @@
+import { INCREASE_ITEM, DECREASE_ITEM, DELETE_ITEM } from '../actions/cart';
+
+const initialState = {
+  items: [],
+};
+
+export const cartReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREASE_ITEM: {
+      return {
+        ...state,
+        items: [...state.items].map((item) => (item.id === action.id ? { ...item, qty: ++item.qty } : item)),
+      };
+    }
+    case DECREASE_ITEM: {
+      return {
+        ...state,
+        items: [...state.items].map((item) => (item.id === action.id ? { ...item, qty: --item.qty } : item)),
+      };
+    }
+    case DELETE_ITEM: {
+      return { ...state, items: [...state.items].filter((item) => item.id !== action.id) };
+    }
+    default: {
+      return state;
+    }
+  }
+};
