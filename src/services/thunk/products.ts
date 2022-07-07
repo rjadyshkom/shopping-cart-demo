cadd89597995e25cc874d770af41546a27ab9e30
rjadyshkom <sergey@rjadysh.com>
2022-07-07 11:52:05 +0300

    fix: promise delay

--- src/services/thunk/products.ts
+++ src/services/thunk/products.ts
@@ -4,7 +4,7 @@ import { makeTimeout } from '../../helpers/constants';
 
 export function getProductsThunk(dispatch: any) {
   dispatch(productsActions.setLoading(true));
-  return Promise.all([ProductsService.getAll(), makeTimeout(3000)])
+  return Promise.all([ProductsService.getAll(), makeTimeout(1500)])
     .then(([response]) => {
       dispatch(productsActions.setProducts(response.data));
       dispatch(productsActions.setLoading(false));
