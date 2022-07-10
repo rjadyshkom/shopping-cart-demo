77fe7343663aee913208e70b3104b865368c8757
rjadyshkom <sergey@rjadysh.com>
2022-07-10 13:28:23 +0300

    feat: products redesign

--- src/components/UI/product-card/ProductCard.tsx
+++ src/components/UI/product-card/ProductCard.tsx
@@ -5,6 +5,7 @@ import { Lightbox } from '../lightbox/Lightbox';
 import { motion } from 'framer-motion';
 import { useDispatch, useSelector } from 'react-redux';
 import * as cartActions from '../../../services/actions/cart';
+import { TrashButton } from '../trash-button/TrashButton';
 
 export const ProductCard = ({ product }: any) => {
   const dispatch = useDispatch();
@@ -20,34 +21,34 @@ export const ProductCard = ({ product }: any) => {
       className={classes.container}
     >
       <Lightbox image={product.image} alt={product.name}>
-        <img draggable={false} className={classes.image} src={product.image} alt={product.name} />
+        <div className={classes.imageWrapper}>
+          <img draggable={false} className={classes.image} src={product.image} alt={product.name} />
+        </div>
       </Lightbox>
-      <div className={classes.specs}>
-        <h2 className={classes.name}>{product.name}</h2>
-        <p className={classes.description}>{product.description}</p>
-        <div className={classes.activities}>
-          {[...product.chips].sort().map((item: string, key: number) => (
-            <span key={key} className={currentCategory === item ? classes.active : classes.activity}>
-              {item}
-            </span>
-          ))}
-        </div>
+      <div className={classes.info}>
+        <span className={classes.id}>{product.id}</span>
+        <span className={classes.price}>{`от ${product.price} р.`}</span>
       </div>
-
-      <div className={classes.wrapper}>
-        <span className={classes.price}>от {product.price} руб.</span>
-        <div className={classes.controls}>
-          {!isExist && (
-            <Button isExist={isExist} onClick={() => dispatch(cartActions.addItem(product))}>
-              Добавить в заявку
-            </Button>
-          )}
-          {isExist && (
-            <Button isExist={isExist} onClick={() => dispatch(cartActions.deleteItem(product))}>
-              Удалить из заявки
-            </Button>
-          )}
-        </div>
+      <h2 className={classes.name}>{product.name}</h2>
+      <div className={classes.activities}>
+        {[...product.chips].sort().map((item: string, key: number) => (
+          <span key={key} className={currentCategory === item ? classes.active : classes.activity}>
+            {item}
+          </span>
+        ))}
+      </div>
+      <div className={classes.controls}>
+        {!isExist && (
+          <Button isExist={isExist} onClick={() => dispatch(cartActions.addItem(product))}>
+            Добавить в заявку
+          </Button>
+        )}
+        {isExist && (
+          <div className={classes.controls}>
+            <Button disabled={isExist} isExist={isExist}>В заявке</Button>
+            <TrashButton onClick={() => dispatch(cartActions.deleteItem(product))} />
+          </div>
+        )}
       </div>
     </motion.div>
   );
