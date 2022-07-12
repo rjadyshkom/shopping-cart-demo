66a592c47c308bb4a110fa6eb76534610e605f15
rjadyshkom <sergey@rjadysh.com>
2022-07-12 11:24:14 +0300

    fix: products component refactoring

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -6,9 +6,6 @@ import { routes } from '../../helpers/routes';
 import { Navigation } from '../UI/navigation/Navigation';
 import { CartQuantity } from '../UI/cart/quantity/CartQuantity';
 import { PopupPlace } from '../UI/popup/PopupPlace';
-import { Products } from '../../pages/products/Products';
-import { Request } from '../../pages/request/Request';
-import { ProductCategories } from '../UI/products/categories/ProductCategories';
 
 function App() {
   return (
