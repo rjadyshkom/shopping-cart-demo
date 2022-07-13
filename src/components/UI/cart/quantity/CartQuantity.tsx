6e1595076509f3f3b3db97a66e70afeb2691c90f
rjadyshkom <sergey@rjadysh.com>
2022-07-13 12:30:36 +0300

    fix: total qty logic

--- src/components/UI/cart/quantity/CartQuantity.tsx
+++ src/components/UI/cart/quantity/CartQuantity.tsx
@@ -6,10 +6,14 @@ import { useSelector } from 'react-redux';
 
 export const CartQuantity = () => {
   const cartItems = useSelector((state: any) => state.cart.items);
+  const cartItemsTotalQty = cartItems.reduce(
+    (previousValue: any, currentItem: any) => previousValue + currentItem.qty,
+    0,
+  );
   return (
     <Link className={classes.link} to={'/request'}>
       <img src={cartIcon} alt="Иконка корзины" />
-      {cartItems.length > 0 && <span className={classes.badge}>{cartItems.length}</span>}
+      {cartItems.length !== 0 && <span className={classes.badge}>{cartItemsTotalQty}</span>}
     </Link>
   );
 };
