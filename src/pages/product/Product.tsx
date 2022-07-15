08c9692460bd56790c75081dba7d6d39b5904d1f
rjadyshkom <sergey@rjadysh.com>
2022-07-15 21:13:18 +0300

    feat: dynamic product page

--- src/pages/product/Product.tsx
+++ src/pages/product/Product.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import classes from './product.module.css';
 import { useParams } from 'react-router-dom';
 import { Loader } from '../../components/UI/products/loader/Loader';
+import { useDispatch, useSelector } from 'react-redux';
+import { productsSelector } from '../../services/selectors/products';
+import { getProductsThunk } from '../../services/thunk/products';
 
 export const Product = () => {
   const { productId }: any = useParams();
+  const { products } = useSelector(productsSelector);
+  const dispatch: any = useDispatch();
+
+  const productData = products.find((product: any) => product.id === productId.split('_').join(' ').toUpperCase());
+
+  useEffect(() => {
+    dispatch(getProductsThunk);
+  }, []);
   return (
     <section className={classes.product}>
-      <h2 className={classes.id}>{productId}</h2>
+      <h2 className={classes.id}>{productData.name}</h2>
       <Loader />
     </section>
   );
